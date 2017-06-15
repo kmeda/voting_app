2548cba164b0fd9706f7fff097265489c8309f4d
Karthik Meda <karthikreddymeda@gmail.com>
2017-06-15 10:04:19 +0530

    initial

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'redux', 'react-redux', 'react-dom',
+  'redux-form', 'redux-thunk', 'react-router-dom','jquery', 'foundation-sites'
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1'
+        }),
+      },
+      {
+  test: /\.scss$/,
+  use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader', 'sass-loader'],
+  })),
+},
+
+      {
+      test: /\.(jpe?g|png|gif|svg)$/,
+      use: [
+        { loader: 'url-loader',
+          options: {limit: 40000}},
+        'image-webpack-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin({
+    filename: 'styles.css',
+    allChunks: true
+  }),
+  new webpack.ProvidePlugin({
+   $: "jquery",
+   jQuery: "jquery"
+ }),
+ new webpack.NamedModulesPlugin()
+  ]
+};
