c8b7daad7c2a92158bf7ce223ab4ed47262419f3
kmeda <karthikreddymeda@gmail.com>
2017-12-05 18:45:38 +0530

    firebase env err

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
 
-if(process.env.NODE_ENV == 'production'){
+if(process.env.NODE_ENV !== 'production'){
+  var config = require('../../config/config').config;
+  console.log("Inside if")
+} else {
   var config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
@@ -9,9 +12,6 @@ if(process.env.NODE_ENV == 'production'){
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
-  
-} else {
-  var config = require('../../config/config').config;
 }
 
 console.log(process.env.NODE_ENV);
