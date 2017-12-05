c2344fcd877c638b6bd42add08c18e761382bb50
kmeda <karthikreddymeda@gmail.com>
2017-12-05 19:03:49 +0530

    firebase env err

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,23 +1,18 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
-if(process.env.NODE_ENV !== 'production'){
-  var config = require('../../config/config').config;
-  console.log("Inside if")
-} else {
+try {
   var config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
-  };
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
+    };
+    firebase.initializeApp(config);
+} catch (e) {
+    console.log(e);
 }
-
-console.log(process.env);
-console.log(config);
-
-firebase.initializeApp(config);
 
 export var twitterProvider = new firebase.auth.TwitterAuthProvider();
 export var firebaseRef = firebase.database().ref();
